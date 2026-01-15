7f051636404b8951e3071f3ff700d91eaccb4b3a
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-01-15 13:29:21 +0800

    feat: Add initial index view pages for barang, kategori, penempatan, ruangan, and users modules.

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -63,7 +63,13 @@
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                 <a href="{{ route('kategori.edit', $k->id) }}"
-                                    class="text-indigo-600 hover:text-indigo-900 mr-3 inline-flex items-center font-semibold">
+                                    class="text-indigo-600 hover:text-indigo-900 mr-3 font-semibold inline-flex items-center">
+                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                                        </path>
+                                    </svg>
                                     Edit
                                 </a>
                                 <x-forms.delete-button :action="route('kategori.destroy', $k->id)" />
